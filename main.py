0469c133d81d7b809ca0c8817d7e7e8fd40c443d
islamic_dua_v3 <barira.ushshaque@gmail.com>
2021-05-17 01:03:03 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print("listening...")
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if "alexa" in command:
+                command = command.replace("alexa", '')
+                print(command)
+    except:
+        pass
+    return command
+
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'who' in command:
+        person = command.replace('who', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'date' in command:
+        talk('sorry, I cant talk to you now. I am angry!')
+    elif 'are you single' in command:
+        talk('I am in a relationship with you')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    else:
+        talk('You dont want to talk to me?')
+
+
+while True:
+    run_alexa()
